afa540610731938c865e888dab121026113d17b8
EC2 Default User <ec2-user@ip-172-31-7-144.ap-south-1.compute.internal>
2025-08-12 03:22:16 +0000

    Jenkins file uploaded

--- myweb/manage.py
+++ myweb/manage.py
@@ -1,22 +1,7 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
+# Input with parameters
+
 import sys
-
-
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myweb.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
+print(" Jenkins build using parameters ! ")
+X_VALUE = int(sys.argv[1])
+Y_VALUE = int(sys.argv[2])
+print(" Multiply= ", (X_VALUE*Y_VALUE))
